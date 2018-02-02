7de5e305d4439dd447728f23b1f8363aa1b2601f
Ubuntu <ubuntu@ip-10-193-8-15.ap-southeast-2.compute.internal>
2018-02-02 12:25:34 +1100

    consourse 1

--- tasks/upload-stemcell/task.sh
+++ tasks/upload-stemcell/task.sh
@@ -27,7 +27,7 @@ function main() {
 
   printf "Uploading %s to %s ...\n" "${stemcell}" "${OPSMAN_HOST}"
 
-  om-linux -t https://${OPSMAN_HOST} -c ${OPSMAN_USERNAME} -s ${OPSMAN_PASSWORD} -k upload-stemcell -s ${stemcell}
+  om-linux -t https://${OPSMAN_HOST} -c ${OPSMAN_CLIENT-ID} -s ${OPSMAN_CLIENT-SECRET} -k upload-stemcell -s ${stemcell}
 
 }
 
